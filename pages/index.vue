f3852c4c13aed45b288c4ce3ab01ad413d65d60f
azcjayce <azc.jayce@gmail.com>
2025-02-06 11:54:25 +0700

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-card-title>Welcome to My SEO-Friendly Site</v-card-title>
+            <v-card-text>
+                This is a sample page built with Nuxt.js and Vuetify.
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
